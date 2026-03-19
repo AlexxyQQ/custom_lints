c96e4394ac69aa616b01bc2e80cad569b660e04e
AlexxyQQ <aayushpandey616@gmail.com>
2026-03-19 20:24:24 +0545

    Removed: - removed hard coded string rule

--- lib/src/app_custom_lints_base.dart
+++ lib/src/app_custom_lints_base.dart
@@ -1,4 +1,3 @@
-import 'package:app_custom_lints/src/lint_rules/hardcoded_string/no_hardcoded_strings_rule.dart';
 import 'package:app_custom_lints/src/lint_rules/sizes/avoid_edge_insets_all.dart';
 import 'package:app_custom_lints/src/lint_rules/sizes/avoid_edge_insets_only.dart';
 import 'package:app_custom_lints/src/lint_rules/sizes/avoid_edge_insets_symmetric.dart';
@@ -6,18 +5,10 @@ import 'package:app_custom_lints/src/lint_rules/sizes/avoid_sized_box_height.dar
 import 'package:app_custom_lints/src/lint_rules/sizes/avoid_sized_box_width.dart';
 import 'package:custom_lint_builder/custom_lint_builder.dart';
 
-/// A custom lint plugin that provides lint rules for analyzing Dart code
 class CustomLintPlugin extends PluginBase {
-  /// Returns a list of lint rules to be applied during analysis
-  ///
-  /// [configs] - Configuration options for the lint rules
-  ///
-  /// Returns a list containing the [NoHardcodedStringsRule]
   @override
   List<LintRule> getLintRules(CustomLintConfigs configs) {
     return [
-      const HardcodedStringLintRule(),
-      // const EvenNumbersOnlyLintRule(),
       AvoidSizedBoxHeight(),
       AvoidSizedBoxWidth(),
       AvoidEdgeInsetsAll(),
@@ -27,25 +18,11 @@ class CustomLintPlugin extends PluginBase {
   }
 }
 
-/// Creates and returns a new instance of the custom lint plugin
-///
-/// This plugin analyzes dart files and raises warnings for string literals
-/// that are declared inside classes extending Widget or State
-///
-/// Returns a [PluginBase] instance configured with the custom lint rules
 PluginBase createPlugin() => _CustomLints();
 
-/// Internal implementation of the custom lint plugin
 class _CustomLints extends PluginBase {
-  /// Creates a new instance of [_CustomLints]
   _CustomLints();
 
-  /// Returns the list of lint rules based on configuration
-  ///
-  /// [configs] - Configuration options for the lint rules
-  ///
-  /// Returns an empty list if linting is disabled, otherwise returns
-  /// a list containing [NoHardcodedStringsRule]
   @override
   List<LintRule> getLintRules(CustomLintConfigs configs) {
     final enabled = configs.enableAllLintRules ?? true;
@@ -54,8 +31,6 @@ class _CustomLints extends PluginBase {
     }
 
     return [
-      const HardcodedStringLintRule(),
-      // const EvenNumbersOnlyLintRule(),
       AvoidSizedBoxHeight(),
       AvoidSizedBoxWidth(),
       AvoidEdgeInsetsAll(),
