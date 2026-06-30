8479f685482fe470a8d41f4ff7d5a1c470ce44d2
AlexxyQQ <aayushpandey616@gmail.com>
2026-06-30 09:54:16 +0545

    example test

---
+++ test/app_custom_lints_test.dart
@@ -0,0 +1,11 @@
+import 'package:test/test.dart';
+
+void main() {
+  group('app_custom_lints', () {
+    test('placeholder test', () {
+      // TODO: Write actual analyzer tests using test_reflective_loader or similar.
+      // Currently, testing via the `example/` project is recommended.
+      expect(true, isTrue);
+    });
+  });
+}
